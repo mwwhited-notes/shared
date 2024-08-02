83e82ea77adaf759b55b51acee472ad854797caa
Matthew Whited <mwhited@eliassen.com>
2024-08-01 20:20:55 -0400

    Add examples

---
+++ Examples/BehaviorBlackboard/KnowledgeSourceHasEmail.cs
@@ -0,0 +1,14 @@
+﻿using BehaviorBlackboard.Engine;
+
+namespace BehaviorBlackboard;
+
+public class KnowledgeSourceHasEmail : IKnowledgeSource
+{
+    public void HandleChange(IBlackboard blackboard, IEventData data)
+    {
+        if (data.Key.Equals("email", StringComparison.OrdinalIgnoreCase))
+        {
+            blackboard.Set(this, "ready", true);
+        }
+    }
+}
